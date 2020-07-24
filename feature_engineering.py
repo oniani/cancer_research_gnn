85492d9e58b469678b844992d39ecb33233bbd87
David  Oniani;Oniani.David@mayo.edu;30005663 <m232032@infodev8.mayo.edu>
2020-07-24 13:52:52 -0500

    Data generation and feature engineering pipelines

---
+++ feature_engineering.py
@@ -0,0 +1,68 @@
+# Feature engineering
+import csv
+
+from collections import Counter  
+
+import pandas as pd
+import numpy as np
+
+from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_recall_fscore_support as score
+
+
+LEN = 244
+
+def main():
+    header = ["id"]
+    feats = []
+    for i in range(242):
+        feat = "feat_" + str(i)
+        header.append(feat)
+        feats.append(feat)
+    header.append("class")
+
+    feats = np.array(feats)
+
+    df = pd.read_csv("temp.content", sep="\t", names=header)
+
+    x_train, x_test, y_train, y_test = train_test_split(df[feats], df["class"], test_size=0.3)
+
+    clf = RandomForestClassifier(n_estimators = 100)
+    clf.fit(x_train, y_train)
+
+    importances = clf.feature_importances_
+    sorted_idx = np.argsort(importances)
+
+    x = list(zip(feats[sorted_idx], importances[sorted_idx]))
+    x_sorted = sorted(x, key=lambda x: -x[1])
+
+    # Statistics
+    y_pred = clf.predict(x_test)
+    precision, recall, fscore, _ = score(y_test, y_pred, average="macro")
+
+    print("Precision:", round(precision, 3))
+    print("Recall:   ", round(recall, 3))
+    print("F-Score:  ", round(fscore, 3))
+    print("Accuracy: ", round((y_pred == y_test).sum() / len(y_pred), 3))
+
+    selected_feats = [key for key, val in x_sorted[:20]]
+    print(selected_feats)
+
+    content = list(csv.reader(open("temp.content"), delimiter="\t"))
+
+    # A hack, but works (:
+    idxs = [int(feat.split("_")[1]) for feat in selected_feats]
+
+    with open("cora.content", "w") as file:
+       content_writer = csv.writer(file, delimiter="\t")
+       for row in content:
+           lst = [row[0]]
+           lst.extend([val for idx, val in enumerate(row) if idx in idxs])
+           lst.extend([row[-1]])
+           content_writer.writerow(lst)
+
+
+if __name__ == "__main__":
+    main()
---
+++ generate_data.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Description: Generate new data based on select few classes
+# Author: David Oniani
+# Date: 17-07-2020
+
+import csv
+import random
+
+def get_class(data, classname):
+    """Gets a list of patients based on a class."""
+
+    return [row for row in data if row[-1] == classname]
+
+
+def main():
+    """The main function."""
+
+    cites = list(csv.reader(open("temp.cites"), delimiter="\t"))
+    content = list(csv.reader(open("temp.content"), delimiter="\t"))
+
+    classes = {'Lung_Cancer': 250,
+               'Colon/Rectum_Cancer': 140}
+
+    limit = min(classes.values())
+
+    data = []
+    for classname in classes:
+        data.extend(random.sample(get_class(content, classname), limit))
+        # data.extend(get_class(content, classname))
+
+    ids = []
+    with open("cora.content", "w") as file:
+       content_writer = csv.writer(file, delimiter="\t")
+       for row in data:
+           ids.append(row[0])
+           content_writer.writerow(row)
+
+
+    with open("cora.cites", "w") as file:
+        cites_writer = csv.writer(file, delimiter="\t")
+        for row in cites:
+            if row[0] in ids and row[1] in ids:
+                cites_writer.writerow(row)
+
+
+if __name__ == "__main__":
+    main()
